5b737e43959969d443d2b803c48823bbc21f9266
Dario Campisi <dariocampisi15@icloud.com>
2023-09-15 20:23:31 +0200

    minor changes

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dariocampisi/università_locale/programmazione/progetto_finale/boids_github
-# Build directory: /Users/dariocampisi/università_locale/programmazione/progetto_finale/boids_github/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(boids.t "/Users/dariocampisi/università_locale/programmazione/progetto_finale/boids_github/build/boids.t")
-set_tests_properties(boids.t PROPERTIES  _BACKTRACE_TRIPLES "/Users/dariocampisi/università_locale/programmazione/progetto_finale/boids_github/CMakeLists.txt;41;add_test;/Users/dariocampisi/università_locale/programmazione/progetto_finale/boids_github/CMakeLists.txt;0;")
